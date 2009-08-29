4c86f4411ea32ddaea41d3e0fbaac26aeadf8720
Alexander Shvetsov <ashvetsov@live.ru>
2009-08-29 13:55:31 +0400

    Implemented RequireTokenAuthorization token; Added Token-based authorization parameter inspector.
    Signed-off-by: Alexander Shvetsov <ashvetsov@live.ru>

---
+++ ServiceModel/App_Code/Authorization/Token/RequireTokenAuthorization.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace Definitif.ServiceModel.Authorization.Token
+{
+    /// <summary>
+    /// Sets Token-based authorization mode for OperationContract.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RequireTokenAuthorization : Attribute, IOperationBehavior
+    {
+        private TokenValidationMode mode = TokenValidationMode.Token;
+
+        /// <summary>
+        /// Sets Token validation mode.
+        /// </summary>
+        public TokenValidationMode Mode
+        {
+            set { this.mode = value; }
+        }
+
+        /// <summary>
+        /// Creates new Token-based authorization requirement using
+        /// default Token validation mode.
+        /// </summary>
+        public RequireTokenAuthorization() { }
+        /// <summary>
+        /// Creates new Token-based authorization requirement using
+        /// given Token validation mode.
+        /// <param name="mode">Token-based authorizatin validation mode.</param>
+        /// </summary>
+        public RequireTokenAuthorization(TokenValidationMode mode)
+        {
+            this.mode = mode;
+        }
+
+        public void AddBindingParameters(OperationDescription operationDescription, BindingParameterCollection bindingParameters)
+        { }
+
+        public void ApplyClientBehavior(OperationDescription operationDescription, ClientOperation clientOperation)
+        { }
+
+        public void ApplyDispatchBehavior(OperationDescription operationDescription, DispatchOperation dispatchOperation)
+        {
+            // Searching for parent authorization inspector and
+            // associating it with new operation parameter inspector.
+            foreach (IDispatchMessageInspector inspector in dispatchOperation.Parent.MessageInspectors)
+            {
+                if (inspector is TokenAuthorizationInspector)
+                {
+                    dispatchOperation.ParameterInspectors.Add(
+                        new TokenAuthorizationParameterInspector(
+                            inspector as TokenAuthorizationInspector, this.mode));
+                    break;
+                }
+            }
+        }
+
+        public void Validate(OperationDescription operationDescription)
+        { }
+    }
+}
---
+++ ServiceModel/App_Code/Authorization/Token/TokenAuthorizationParameterInspector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+using System.ServiceModel.Dispatcher;
+
+namespace Definitif.ServiceModel.Authorization.Token
+{
+    /// <summary>
+    /// Represets Token-base authorization operation parameters inspector.
+    /// </summary>
+    public class TokenAuthorizationParameterInspector : IParameterInspector
+    {
+        private TokenAuthorizationInspector parent;
+        private TokenValidationMode mode;
+
+        /// <summary>
+        /// Creates new parameter inspector for Token-based authorization.
+        /// </summary>
+        /// <param name="parent">Token-based authorization service message inspector.</param>
+        /// <param name="mode">Token-based authorizatin validation mode.</param>
+        public TokenAuthorizationParameterInspector(TokenAuthorizationInspector parent, TokenValidationMode mode)
+        {
+            this.parent = parent;
+            this.mode = mode;
+        }
+
+        public object BeforeCall(string operationName, object[] inputs)
+        {
+            if (!parent.ValidateContext(HttpContext.Current, this.mode))
+            {
+                throw new WebException(System.Net.HttpStatusCode.Unauthorized, "");
+            }
+            return null;
+        }
+
+        public void AfterCall(string operationName, object[] outputs, object returnValue, object correlationState)
+        { }
+    }
+}
