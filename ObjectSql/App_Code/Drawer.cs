770999ed889283973850c52cf11e11a432fabe6e
Alexander Shvetsov <ashvetsov@live.ru>
2009-09-05 22:57:37 +0400

    Added Compiled option to regular expression in drawer.
    Signed-off-by: Alexander Shvetsov <ashvetsov@live.ru>

--- ObjectSql/App_Code/Drawer.cs
+++ ObjectSql/App_Code/Drawer.cs
@@ -479,7 +479,7 @@ namespace Definitif.Data.ObjectSql
                     // Numeric values can be not surrounded with '',
                     // but string ones must, so we will also replace
                     // ' char to double one ''.
-                    if (!Regex.IsMatch(result, @"^[\d\.]+$"))
+                    if (!Regex.IsMatch(result, @"^[\d\.]+$", RegexOptions.Compiled))
                     {
                         result = "'" + result.Replace("'", "''") + "'";
                     }
