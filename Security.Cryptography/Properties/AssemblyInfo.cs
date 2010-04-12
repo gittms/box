a217e7dc2247a5aa3779cb375e8f127e6cdf89db
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-04-12 16:39:46 +0400

    Added Definitif.Security.Cryptography and Definitif.ServiceModel projects to solution.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

--- Security.Cryptography/Properties/AssemblyInfo.cs
+++ Security.Cryptography/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Definitif")]
 [assembly: AssemblyProduct("Definitif.Security.Cryptography")]
-[assembly: AssemblyCopyright("Copyright © Definitif 2009")]
+[assembly: AssemblyCopyright("Copyright © Definitif 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("cb6b7f89-9c57-4069-9002-13e2967820ca")]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
