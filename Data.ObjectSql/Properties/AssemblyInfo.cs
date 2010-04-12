6ac1c5592136721c4c68dbb8fd54c6088d3208e9
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-04-12 16:34:05 +0400

    Added Definitif.Data.ObjectSql project and test project to solution.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

--- Data.ObjectSql/Properties/AssemblyInfo.cs
+++ Data.ObjectSql/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Definitif")]
 [assembly: AssemblyProduct("Definitif.Data.ObjectSql")]
-[assembly: AssemblyCopyright("Copyright © Definitif 2009")]
+[assembly: AssemblyCopyright("Copyright © Definitif 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("a101788a-0123-4499-95b6-292483631fed")]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
