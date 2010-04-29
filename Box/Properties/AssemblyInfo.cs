b668d1d16c159c548ad5489f556d357d7f5e47c5
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-04-29 14:29:24 +0400

    Added Definitif.Box class library for misc extensions providing; Implemented NamedFormat for strings.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

---
+++ Box/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Definitif.Box")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Definitif")]
+[assembly: AssemblyProduct("Definitif.Box")]
+[assembly: AssemblyCopyright("Copyright © Definitif 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("782580fb-f012-42ba-9f3e-619f278c0953")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Box/String.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Definitif.Box;
+
+namespace Definitif
+{
+    public static class StringEntensions
+    {
+        /// <summary>
+        /// Formats string using provided data dynamic object.
+        /// </summary>
+        /// <returns>Formatted string.</returns>
+        public static String F(this String str, object data)
+        {
+            return NamedFormat.FormatWith(str, data);
+        }
+
+        /// <summary>
+        /// Indents string by given number of spaces.
+        /// </summary>
+        /// <returns>Indented string.</returns>
+        public static string Indent(this string str, int spaces)
+        {
+            string indent = "".PadLeft(spaces),
+                result = "";
+
+            foreach (string line in str.Split(new string[] { Environment.NewLine }, StringSplitOptions.None))
+            {
+                if (result != "") result += Environment.NewLine;
+                result += indent + line;
+            }
+
+            return result;
+        }
+    }
+}
