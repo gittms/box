d62febe36721d9b53a4bd859790e217f74ae680b
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-04-29 17:05:46 +0400

    Implemented JsonExtensions for dynamic JSON serialization and typed deserialization.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

---
+++ Box/Json.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Definitif
+{
+    public static class JsonExtensions
+    {
+        /// <summary>
+        /// Serializes object to JSON representation.
+        /// </summary>
+        /// <returns>JSON representation of object.</returns>
+        public static string ToJson(this object obj)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                var ser = new DataContractJsonSerializer(obj.GetType());
+                ser.WriteObject(ms, obj);
+                return Encoding.Default.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Deserializes string to object of given type.
+        /// </summary>
+        /// <typeparam name="T">Type to deserealize.</typeparam>
+        /// <returns>Deserialized object.</returns>
+        public static T FromJson<T>(this string str)
+        {
+            using (var ms = new MemoryStream(Encoding.Default.GetBytes(str.ToCharArray())))
+            {
+                var ser = new DataContractJsonSerializer(typeof(T));
+                return (T)ser.ReadObject(ms);
+            }
+        }
+    }
+}
