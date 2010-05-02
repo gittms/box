fb186ecda0f11dcd375a3bd935f0dd5197897dc4
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-05-02 13:09:19 +0400

    Added draft and non-working implementation of MySql databases support to Data.ObjectSql project.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

---
+++ Data.ObjectSql/App_Code/Implementation/MySql/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Definitif.Data.ObjectSql.Implementation.MySql
+{
+    public sealed class Database : ObjectSql.Database
+    {
+        protected override ObjectSql.Drawer GetDrawer()
+        {
+            return new Drawer();
+        }
+
+        public override IDbConnection GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IDbCommand GetCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateSchema()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data.ObjectSql/App_Code/Implementation/MySql/Drawer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Definitif.Data.ObjectSql.Implementation.MySql
+{
+    public sealed class Drawer : ObjectSql.Drawer
+    {
+        public override string IDENTITY
+        {
+            get { return "SELECT LAST_INSERT_ID()"; }
+        }
+    }
+}
