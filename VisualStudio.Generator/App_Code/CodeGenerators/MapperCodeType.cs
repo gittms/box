112784e61c26f6a0902b05e27af93b4dfe938ed2
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-05-02 16:06:35 +0400

    Implemented field prefix argument support for ReadObject generated methods [#2 state:resolved].
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

--- VisualStudio.Generator/App_Code/CodeGenerators/MapperCodeType.cs
+++ VisualStudio.Generator/App_Code/CodeGenerators/MapperCodeType.cs
@@ -21,7 +21,9 @@ namespace Definitif.VisualStudio.Generator
             codeType.BaseTypes.Add(String.Format("Mapper<{0}.{1}>", modelsNamespace.Name, model.Name));
 
             // Generating strings for values reading and assignement.
-            List<string> values = new List<string>(), reads = new List<string>();
+            List<string> values = new List<string>(),
+                reads = new List<string>(),
+                readsWithPrefix = new List<string>();
             foreach (Member member in model.Members)
             {
                 if (String.IsNullOrWhiteSpace(member.ColumnName)) continue;
@@ -39,10 +41,12 @@ namespace Definitif.VisualStudio.Generator
                 //     table["column"] == (obj.Name == Id.Empty) ? DBNull.Value : obj.Name
                 //   SELECT:
                 //     Name = (reader["column"] == DBNull.Value) ? Id.Empty : new Id(reader["column"])
+                //     Name = (reader[fieldPrefix + "column"] == DBNull.Value) ? Id.Empty : new Id(reader[fieldPrefix + "column"])
                 if (member.Type == "Id")
                 {
                     values.Add(@"table[""{column}""] == (obj.{name} == Id.Empty) ? DBNull.Value : obj.{name},".F(replacement));
                     reads.Add(@"{name} = (reader[""{column}""] == DBNull.Value) ? Id.Empty : new Id(reader[""{column}""]),".F(replacement));
+                    readsWithPrefix.Add(@"{name} = (reader[fieldPrefix + ""{column}""] == DBNull.Value) ? Id.Empty : new Id(reader[fieldPrefix + ""{column}""]),".F(replacement));
                 }
                 // When member is foreign key, so it is represented
                 // by Id and typed properties:
@@ -50,30 +54,36 @@ namespace Definitif.VisualStudio.Generator
                 //     table["column"] == (obj.NameId == Id.Empty) ? DBNull.Value : obj.Name
                 //   SELECT:
                 //     NameId = (reader["column"] == DBNull.Value) ? Id.Empty : new Id(reader["column"])
+                //     NameId = (reader[fieldPrefix + "column"] == DBNull.Value) ? Id.Empty : new Id(reader[fieldPrefix + "column"])
                 else if ((member.Modifiers & Modifier.Foreign_key) != 0)
                 {
                     values.Add(@"table[""{column}""] == (obj.{name}Id == Id.Empty) ? DBNull.Value : obj.{name},".F(replacement));
                     reads.Add(@"{name}Id = (reader[""{column}""] == DBNull.Value) ? Id.Empty : new Id(reader[""{column}""]),".F(replacement));
+                    readsWithPrefix.Add(@"{name}Id = (reader[fieldPrefix + ""{column}""] == DBNull.Value) ? Id.Empty : new Id(reader[fieldPrefix + ""{column}""]),".F(replacement));
                 }
                 // When member have custom casting type specified:
                 //   INSERT:
                 //     table["column"] == (cast)obj.Name
                 //   SELECT:
                 //     Name = (type)((cast)reader["column"])
+                //     Name = (type)((cast)reader[fieldPrefix + "column"])
                 else if (!String.IsNullOrWhiteSpace(member.ColumnCastingType))
                 {
                     values.Add(@"table[""{column}""] == ({cast})obj.{name},".F(replacement));
                     reads.Add(@"{name} = ({type})(({cast})reader[""{column}""]),".F(replacement));
+                    readsWithPrefix.Add(@"{name} = ({type})(({cast})reader[fieldPrefix + ""{column}""]),".F(replacement));
                 }
                 // When member is just a mapped member:
                 //   INSERT:
                 //     table["column"] == obj.Name
                 //   SELECT:
                 //     Name = (type)reader["column"]
+                //     Name = (type)reader[fieldPrefix + "column"]
                 else
                 {
                     values.Add(@"table[""{column}""] == obj.{name},".F(replacement));
                     reads.Add(@"{name} = ({type})reader[""{column}""],".F(replacement));
+                    readsWithPrefix.Add(@"{name} = ({type})reader[fieldPrefix + ""{column}""],".F(replacement));
                 }
             }
 
@@ -84,7 +94,8 @@ namespace Definitif.VisualStudio.Generator
                 database = String.IsNullOrWhiteSpace(model.DatabaseRef) ? Constants.DefaultDatabase : model.DatabaseRef,
                 table = model.TableName,
                 values = String.Join(Environment.NewLine, values.ToArray()).Indent(7 * 4).Trim(),
-                reads = String.Join(Environment.NewLine, reads.ToArray()).Indent(4 * 4).Trim()
+                reads = String.Join(Environment.NewLine, reads.ToArray()).Indent(4 * 4).Trim(),
+                readsWithPrefix = String.Join(Environment.NewLine, readsWithPrefix.ToArray()).Indent(4 * 4).Trim(),
             };
 
             // Mapper constructor initializes database and
@@ -105,6 +116,15 @@ namespace Definitif.VisualStudio.Generator
                 {reads}
             }};
             FillBase(result, reader);
+            return result;
+        }}
+        public sealed override {modelNamespace}.{type} ReadObject(IDataReader reader, string fieldPrefix)
+        {{
+            {modelNamespace}.{type} result = new {modelNamespace}.{type}()
+            {{
+                {readsWithPrefix}
+            }};
+            FillBase(result, reader, fieldPrefix);
             return result;
         }}".F(variables) + Environment.NewLine));
 
