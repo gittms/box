a279fa546ead0e1060071c9130587ea08da3e27b
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-05-10 17:11:56 +0400

    Added Visual Studio Box Library project template to solution [#10].
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

---
+++ VisualStudio.ProjectTemplate/Core.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Definitif;
+using Definitif.Data;
+
+/// <summary>
+/// Represents Definitif Box Library core.
+/// </summary>
+[ConnectionStringKey("Core")]
+public class Core : Definitif.Core
+{
+    /// <summary>
+    /// Statically initializes core singleton.
+    /// </summary>
+    static Core()
+    {
+        core = new Definitif.Core(typeof(Core));
+    }
+}
---
+++ VisualStudio.ProjectTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Definitif.VisualStudio.ProjectTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Definitif")]
+[assembly: AssemblyProduct("Definitif.Box")]
+[assembly: AssemblyCopyright("Copyright © Definitif 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
