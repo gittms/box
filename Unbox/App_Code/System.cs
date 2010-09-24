61a314b415b7ecb1300d3a1a91ab1132cb3721bc
Alexander Shvetsov <ashvetsov@definitif.ru>
2010-09-24 20:07:04 +0400

    Implemented methods for checking current OS and Runtime.
    Signed-off-by: Alexander Shvetsov <ashvetsov@definitif.ru>

---
+++ Unbox/App_Code/System.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NDesk.Options;
+
+namespace Definitif.Box.Unbox
+{
+    /// <summary>
+    /// Represents static class for system information retrieval.
+    /// </summary>
+    public static class System
+    {
+        /// <summary>
+        /// Checking if running under Mono runtime.
+        /// </summary>
+        public static bool IsMonoRuntime
+        {
+            get
+            {
+                Type t = Type.GetType("Mono.Runtime");
+                return (t != null);
+            }
+        }
+
+        /// <summary>
+        /// Checking if running on Windows platform.
+        /// </summary>
+        public static bool IsWindowsOs
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return ((p == 4) || (p == 6) || (p == 128));
+            }
+        }
+    }
+}
